9fcb3a39984db2d26b83e6bc29e288c1ad6d06e6
yuxh <14136279+yu-xinghang@user.noreply.gitee.com>
2025-04-05 14:03:04 +0800

    Linux changes

--- DEBUG.h
+++
@@ -1,6 +0,0 @@
-int UUUU=0;
-#define db(x) UUUU++;
-#define cendl UUUU++;
-#define multicase UUUU++;
-void pa(auto a){UUUU++;}
-void pa(int *p,int n){UUUU++;}
--- D_Drunken_Maze.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-#include <windows.h>
-void Atomatic_AC_machine(){
-    
-}   
-signed main(){
-        Beep(1000, 1000);
-    return 0;
-}
